4c4919a38f9166582bc94a75dc047ef3afdf88cb
joelee <as955133@gmail.com>
2018-02-08 13:36:44 +0800

    WebAPI教學

---
+++ KendoUIApp1/Controllers/JoinController.cs
@@ -0,0 +1,27 @@
+﻿using Kendo.Mvc.UI;
+using KendoUIApp1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KendoUIApp1.Controllers
+{
+    public class JoinController : Controller
+    {
+        // GET: Join
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        SQLLabEntities se = new SQLLabEntities();
+
+        public ActionResult Test_Read([DataSourceRequest]DataSourceRequest)
+        {
+            return View();
+        }
+    }
+}
