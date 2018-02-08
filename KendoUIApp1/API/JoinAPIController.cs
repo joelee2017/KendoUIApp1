29a477e0db3e203e5835a28c558696c042567544
joelee <as955133@gmail.com>
2018-02-08 14:53:03 +0800

    EF CRUD

--- KendoUIApp1/API/JoinAPIController.cs
+++ KendoUIApp1/API/JoinAPIController.cs
@@ -17,5 +17,43 @@ namespace KendoUIApp1.API
         {
             return db.test1;
         }
+
+        [HttpGet]
+        public IEnumerable<test1> CreateAll( string name, string dept)
+        {
+            test1 t1 = new test1();
+
+            t1.name = name;
+            t1.dept = dept;
+
+            db.test1.Add(t1);
+            db.SaveChanges();
+
+            return db.test1;
+        }
+
+        [HttpGet]
+        public IEnumerable<test1>EditAll(int id)
+        {
+            test1 t1 = db.test1.Find(id);
+
+            t1.name = "wait10";
+            t1.dept = "wait10";
+            
+            db.SaveChanges();
+
+            return db.test1;            
+        }
+
+        [HttpGet]
+        public IEnumerable<test1>DeleteAll(int id)
+        {
+            test1 t1 = db.test1.Find(id);
+
+            db.test1.Remove(t1);
+            db.SaveChanges();
+
+            return db.test1;
+        }
     }
 }
